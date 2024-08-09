ba1837be06b9f2f1cc2aa36136b92fd5e2906b8a
Allysson723 <0063398@academico.ifmg.edu.br>
2024-08-09 08:52:12 -0300

    trabalho prático basicamente terminado, ainda falta otimização, comentários e decoração

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ int main() {
     cout << "Informe a distancia inicial entre o Seu Jose e a Dona Maria: " << endl;
     cin >> distanciaInicial;
 
-    cout << "Informe a velocidade do Seu Jose" << endl;
+    cout << "Informe a velocidade do Seu Jose: " << endl;
     cin >> velocidadeS;
 
-    cout << "Informe a velocidade da Dona Maria" << endl;
+    cout << "Informe a velocidade da Dona Maria: " << endl;
     cin >> velocidadeD;
 
-    bool fubaResgatado = false;
+    //O tamanho será a distancia entre Jose e Maria + 1 para cada automovel + 1 para fuba - 1 pois comeca em 0
     int tamanhoTotal = distanciaInicial + 2;
     random_device rd;
     mt19937 gerador(rd());
@@ -26,54 +26,75 @@ int main() {
     int posicaoS = 0;
     int posicaoD = tamanhoTotal;
 
+    bool joseEncontrou = true;
+    bool fubaResgatado = false;
+    int tempoBusca = 0;
     while (!fubaResgatado) {
+        tempoBusca++;
         uniform_int_distribution<> movimentoFuba(1, 3);
         bernoulli_distribution direcaoFuba(0.5);
         for (int i = 0; i < tamanhoTotal + 1; i++) {
-            if (i == posicaoFuba) {
-                /*if (posicaoFuba <= posicaoS + 1 && posicaoS + 1 < posicaoD) {
-                    posicaoFuba = posicaoS + 1;
-                } else if (posicaoFuba >= posicaoD + 1 && posicaoD - 1 > posicaoS) {
-                    posicaoFuba = posicaoS - 1;
-                }*/
+            if (i < posicaoS || i > posicaoD) {
+                cout << "-" << " ";
+            } else if (i == posicaoS) {
+                cout << "S" << " ";
+            } else if (i == posicaoFuba) {
                 cout << "F" << " ";
             } else if ((i > posicaoS && i < posicaoFuba) || (i > posicaoFuba && i < posicaoD)) {
                 cout << "." << " ";
-            } else if (i == posicaoS) {
-                cout << "S" << " ";
-            } else if (i < posicaoS || i > posicaoD) {
-                cout << "-" << " ";
             } else if (i == posicaoD) {
                 cout << "D" << " ";
             }
         }
+        //Quebra de linha
+        cout << endl;
         if (posicaoFuba <= posicaoS + 1 || posicaoFuba >= posicaoD - 1) {
             fubaResgatado = true;
         }
-        cout << endl;
         bool esquerda = direcaoFuba(gerador);
         if (esquerda) {
             posicaoFuba -= movimentoFuba(gerador);
         } else {
             posicaoFuba += movimentoFuba(gerador);
         }
-        if (posicaoFuba <= posicaoS + velocidadeS) {
-            posicaoS = posicaoFuba - posicaoS - 1;
+        //Caso o movimento aleatório de Fuba ultrapasse algum dos automoveis a posicao dele é definida para logo ao lado do automovel
+        if (posicaoFuba <= posicaoS) {
             posicaoFuba = posicaoS + 1;
             if (posicaoD <= posicaoFuba) {
                 posicaoD = posicaoFuba + 1;
+            } else {
+                posicaoD -= velocidadeD;
             }
-        } else if(posicaoFuba >= posicaoD - velocidadeD){
-            posicaoD = posicaoD - posicaoFuba - 1;
-            posicaoFuba = posicaoS - 1;
+        } else if (posicaoFuba <= posicaoS + velocidadeS) {
+            posicaoS = posicaoFuba - 1;
+            if (posicaoD <= posicaoFuba) {
+                posicaoD = posicaoFuba + 1;
+            } else {
+                posicaoD -= velocidadeD;
+            }
+        } else if (posicaoFuba >= posicaoD) {
+            joseEncontrou = false;
+            posicaoFuba = posicaoD - 1;
             if (posicaoS >= posicaoFuba) {
                 posicaoS = posicaoFuba - 1;
+            } else {
+                posicaoS += velocidadeS;
+            }
+        } else if (posicaoFuba >= posicaoD - velocidadeS) {
+            joseEncontrou = false;
+            posicaoD = posicaoFuba + 1;
+            if (posicaoD <= posicaoFuba) {
+                posicaoD = posicaoFuba + 1;
+            } else {
+                posicaoD -= velocidadeD;
             }
         } else {
             posicaoS += velocidadeS;
             posicaoD -= velocidadeD;
         }
     }
+
+    cout << "Após " << tempoBusca << " minutos de busca, " << (joseEncontrou ? "Jose " : "Maria ") << "encontrou Fuba!" << endl;
 
     return 0;
 }
