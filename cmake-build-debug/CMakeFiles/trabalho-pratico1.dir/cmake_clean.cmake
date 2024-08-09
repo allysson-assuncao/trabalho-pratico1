2084afca7930e66037badb869c16a94303005c9c
Allysson723 <0063398@academico.ifmg.edu.br>
2024-08-09 08:52:20 -0300

    trabalho prático basicamente terminado, ainda falta otimização, comentários e decoração

---
+++ cmake-build-debug/CMakeFiles/trabalho-pratico1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabalho-pratico1.dir/main.cpp.o"
+  "CMakeFiles/trabalho-pratico1.dir/main.cpp.o.d"
+  "trabalho-pratico1"
+  "trabalho-pratico1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trabalho-pratico1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
